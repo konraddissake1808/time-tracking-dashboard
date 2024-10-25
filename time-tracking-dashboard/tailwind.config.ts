3518753d5cf891128407538bb592bdda8d77be9d
konraddissake1808 <konrad.dissakengando@gmail.com>
2024-10-25 16:01:16 +0100

    bugged

--- time-tracking-dashboard/tailwind.config.ts
+++ time-tracking-dashboard/tailwind.config.ts
@@ -9,8 +9,21 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        //primary
+        blue: 'hsl(246, 80%, 60%)',
+        lightRedWork: 'hsl(15, 100%, 70%)',
+        softBluePlay: 'hsl(195, 74%, 62%)',
+        lightRedStudy: 'hsl(348, 100%, 68%)',
+        limeGreenExercise: 'hsl(145, 58%, 55%)',
+        violetSocial: 'hsl(264, 64%, 52%)',
+        softOrangeSelfCare: 'hsl(43, 84%, 65%)',
+
+        //neutral
+        veryDarkBlue: 'hsl(226, 43%, 10%)',
+        darkBlue: 'hsl(235, 46%, 20%)',
+        desaturatedBlue: 'hsl(235, 45%, 61%)',
+        paleBlue: 'hsl(236, 100%, 87%)',
+        White: '#fff',
       },
     },
   },
